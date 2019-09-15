023aa313ea441f77bd8fdd98180a54b4428b2315
Kaycee <thekg85@gmail.com>
2019-09-15 19:57:44 +0200

    added the diamond

--- diamond.js
+++ diamond.js
@@ -1,6 +1,8 @@
-function drawDiamond(){
-    var c = document.getElementById("Canvas");
-    var ctx = c.getContext("2d");
+var c = document.getElementById("Canvas");
+var ctx = c.getContext("2d");
+
+function drawDiamond() {
+    ctx.fillStyle = "#d11313";
     ctx.beginPath();
     ctx.moveTo(300, 200);
     ctx.lineTo(150, 400);
@@ -8,5 +10,11 @@ function drawDiamond(){
     ctx.lineTo(150, 0);
     ctx.lineTo(0, 200);
     ctx.lineTo(150, 400);
-    ctx.stroke();
+    ctx.fill();
+    console.log("Diamond is drawn");
+}
+
+function clearCanvas() {
+    ctx.clearRect(0,0,c.width,c.height);
+    console.log("Diamond is erased");
 }
