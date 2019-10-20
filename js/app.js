d24554458c9a9e24450d50d7609a39e964781914
Kaycee <thekg85@gmail.com>
2019-10-20 18:12:08 +0200

    Nearly finished Beroepsbeeld and made lightswitch

--- js/app.js
+++ js/app.js
@@ -1,11 +1,28 @@
-function lightSwitch() {
-    let color = ["#111", "#fff"];
-    let i = 0;
+window.addEventListener("load", init);
 
-    document.querySelector("button").addEventListener("click", function() {
-    i = i < color.length ? ++i : 0;
-    document.querySelector("html").style.background = color[i];
-    })
+function init() {
+    console.log("Page loaded.");
+    const button = document.getElementById("lightSwitch");
+    button.addEventListener("click", lightSwitch);
 }
 
-lightSwitch();
+function lightSwitch(event) {
+    const html = document.getElementById("html");
+    const button = document.getElementById("lightSwitch");
+
+    if (html.className === "bg-black") {
+        console.log("light");
+        html.className =  "bg-white";
+        button.className = "dark-switch";
+        button.innerText = "Dark";
+        return
+    } else if (html.className === "bg-white") {
+        console.log("dark");
+        html.className = "bg-black";
+        button.className = "light-switch";
+        button.innerText = "Light";
+        return
+    } else {
+        console.log("Unknown class.");
+    }
+}
